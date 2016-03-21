206168ed19f7720d28a590b5d5bcb7401b4a39d7
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-03-22 03:26:22 +0800

    add static files

---
+++ xyw/js/functions_dev.js
@@ -0,0 +1,46 @@
+// variables
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			$ele.css('visibility','visible');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 100);
+		});
+		return this;
+	};
+})(jQuery);
+
+
+
+
+
+function adjustWordsPosition() {
+	$('#words').css("position", "absolute");
+	$('#words').css("top", $("#garden").position().top + 195);
+	$('#words').css("left", $("#garden").position().left + 70);
+}
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+}
+
+function showLoveU() {
+	$('#loveu').fadeIn(3000);
+}
