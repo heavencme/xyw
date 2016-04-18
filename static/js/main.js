dd014b9c51fe38768c3bc838f2577151d165e21a
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-04-19 00:03:30 +0800

    askme page

--- static/js/main.js
+++ static/js/main.js
@@ -31,7 +31,7 @@ $('#modal-bug-submit').click(function(e){
     method: "POST",
     data: { 
         timeStamp: now.getTime(),
-        time: now.toLocaleString().replace(/GMT\+8/g, ''),
+        time: now.Format("yyyy-MM-dd hh:mm:ss"),
         msg: $('#modal-bug-text').val(),
         passCode: $('#password').val()
     },
@@ -186,6 +186,28 @@ function genPage(dairyArr) {
 function isArray(obj) {
     return Object.prototype.toString.call(obj) === '[object Array]';
 }
+
+Date.prototype.Format = function(fmt) {
+    var o = {   
+        "M+" : this.getMonth()+1,                 //月份   
+        "d+" : this.getDate(),                    //日   
+        "h+" : this.getHours(),                   //小时   
+        "m+" : this.getMinutes(),                 //分   
+        "s+" : this.getSeconds(),                 //秒   
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度   
+        "S"  : this.getMilliseconds()             //毫秒   
+    }; 
+
+    if(/(y+)/.test(fmt)) ｛ 
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
+    ｝
+    for(var k in o) ｛ 
+        if(new RegExp("("+ k +")").test(fmt)) ｛
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+        ｝
+    ｝
+    return fmt;   
+} 
 
 /** end **/
 })(jQuery);
