d4f1f8907d793f3fd47c19dc28d2f13a76e35b33
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-04-22 00:43:57 +0800

    test socket.io

---
+++ static/js/stats.js
@@ -0,0 +1,15 @@
+var socket = io.connect('/');
+socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+});
+
+socket.on('connect', function(){
+    console.log('connect');
+});
+socket.on('event', function(data){
+    console.log('event');
+});
+socket.on('disconnect', function(){
+    console.log('disconnect');
+});
