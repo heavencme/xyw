372a512112858066b9285137d6eeaf31a1af27f5
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-04-24 18:06:22 +0800

    raibow 6

--- static/js/main.js
+++ static/js/main.js
@@ -285,7 +285,7 @@ function regPageLink(target, appendTar){
                 $("#page-link").children()[i].remove();
                 g_pageLinkIndex --;
                 if( g_pageLinkIndex < 0){
-                    g_pageLinkIndex = g_rainbow.length - 1;
+                    g_pageLinkIndex = g_rainbow.length;
                 }
 
                 
